1ed9e07f6276c07a249dd86fe42fa02af3b95326
Scott J. Crouch <scottjcrouch@gmail.com>
2017-01-18 17:24:24 +1100

    Controller header formatting

--- src/Controller.h
+++ src/Controller.h
@@ -5,27 +5,27 @@
 
 class Controller {
 public:
-  enum Button {
-    BUTTON_A,
-    BUTTON_B,
-    BUTTON_SELECT,
-    BUTTON_START,
-    BUTTON_UP,
-    BUTTON_DOWN,
-    BUTTON_LEFT,
-    BUTTON_RIGHT,
-    BUTTON_TOTAL
-  };
+    enum Button {
+	BUTTON_A,
+	BUTTON_B,
+	BUTTON_SELECT,
+	BUTTON_START,
+	BUTTON_UP,
+	BUTTON_DOWN,
+	BUTTON_LEFT,
+	BUTTON_RIGHT,
+	BUTTON_TOTAL
+    };
 
-  void setStrobe(bool b);
-  uint8_t poll();
-  void press(Button b);
-  void release(Button b);
+    void setStrobe(bool b);
+    uint8_t poll();
+    void press(Button b);
+    void release(Button b);
 
 private:
-  bool strobe = false;
-  bool buttons[BUTTON_TOTAL] = {false};
-  int shiftReg = -1;
+    bool strobe = false;
+    bool buttons[BUTTON_TOTAL] = {false};
+    int shiftReg = -1;
 };
 
 #endif
