60ee5a2cfc7ed87ef8bfcb4876b6a543ef118082
Scott J. Crouch <scottjcrouch@gmail.com>
2017-01-21 10:59:43 +1100

    Reorder ppu header

--- src/PPU.h
+++ src/PPU.h
@@ -79,17 +79,17 @@ private:
 	0x09, 0x01, 0x34, 0x03, 0x00, 0x04, 0x00, 0x14,
 	0x08, 0x3A, 0x00, 0x02, 0x00, 0x20, 0x2C, 0x08};
 
-    // registers
+    // REGISTERS
 
+    // latch used for SCROLL, ADDR. Unset upon STATUS read
+    bool latch;
     // 0x2000: CTRL
     int nameTableAddr;          // 0=0x2000, 1=0x2400, 2=0x2800, 3=0x2C00
     bool vRAMAddrIncr;          // 0=1 (across name table), 1=32 (down name table)
     bool sprPatternTableAddr;   // 0=0x0000, 1=0x1000, ignored if sprSize is 8x16
     bool bgPatternTableAddr;    // 0=0x0000, 1=0x1000
     bool sprSize;               // 0=8x8, 1=8x16
-    // unused bit
     bool nmiOnVBlank;
-
     // 0x2001: MASK
     bool grayscale;             // mirrors palette to only shades of grey
     bool imageMask;             // show bg in left 8 columns of screen
@@ -99,31 +99,19 @@ private:
     bool buffRed;               // dims non red colours
     bool buffGreen;             // dims non green colours
     bool buffBlue;              // dims non blue colours
-
     // 0x2002: STATUS
-    // 5 unused bits
     bool sprOverflow;           // if > 8 sprites on any scanline in current frame
     bool spr0Hit;               // at first non-zero pixel overlap in both spr0 and bg
     bool isVBlank;              // set during VBlank
-
     // 0x2003: OAMADDR
     uint8_t oamAddrBuffer;
-
     // 0x2005: SCROLL
     uint8_t scrollX;
     uint8_t scrollY;
-
     // 0x2006: ADDR
     uint16_t addrBuffer;
-
     // 0x2007: DATA
     uint8_t readBuffer;
-
-    // latch used for SCROLL, ADDR. Unset upon STATUS read
-    bool latch;
-
-    // rendering
-    uint32_t frameBuffer[FRAME_WIDTH * FRAME_HEIGHT];
 
     int clockCounter;
     int frameCounter;
@@ -131,15 +119,11 @@ private:
     bool spr0Latch;
     bool spr0Reload;
 
-    // background objects
+    uint32_t frameBuffer[FRAME_WIDTH * FRAME_HEIGHT];
     Pixel bgPixels[512][480];
     Tile bgTiles[4][32][30];
     Metatile bgMetaTiles[4][8][8];
-
-    // sprites
     Sprite sprites[64];
-
-    // palette
     Palette palette;
 };
 
