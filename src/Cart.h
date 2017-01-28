22014c40fb2f449f6650e2279da1af394522d396
Scott J. Crouch <scottjcrouch@gmail.com>
2017-01-28 13:24:22 +1100

    Fix cart header indentation

--- src/Cart.h
+++ src/Cart.h
@@ -12,35 +12,35 @@ static const int RAM_BANK_SIZE = 8 * 1024;
 
 class Cart {
 public:
-  bool readFile(std::string romFileName);
-  uint8_t readPrg(int index);
-  void writePrg(int index, uint8_t value);
-  uint8_t readChr(int index);
-  void writeChr(int index, uint8_t value);
-  uint8_t readRam(int index);
-  void writeRam(int index, uint8_t value);
+    bool readFile(std::string romFileName);
+    uint8_t readPrg(int index);
+    void writePrg(int index, uint8_t value);
+    uint8_t readChr(int index);
+    void writeChr(int index, uint8_t value);
+    uint8_t readRam(int index);
+    void writeRam(int index, uint8_t value);
 
-  enum Mirroring {
-      MIRROR_VERT,
-      MIRROR_HOR,
-      MIRROR_ALL,
-      MIRROR_NONE,
-  };
+    enum Mirroring {
+	MIRROR_VERT,
+	MIRROR_HOR,
+	MIRROR_ALL,
+	MIRROR_NONE,
+    };
 
-  Mirroring mirroring;
+    Mirroring mirroring;
   
 private:
-  int prgLen;
-  int chrLen;
-  int ramLen;
-  bool isRamBattery;
-  bool isTrainer;
-  int mapperNum;
+    int prgLen;
+    int chrLen;
+    int ramLen;
+    bool isRamBattery;
+    bool isTrainer;
+    int mapperNum;
 
-  uint8_t trainer[512] = {0};
-  std::unique_ptr<uint8_t[]> prg;
-  std::unique_ptr<uint8_t[]> chr;
-  std::unique_ptr<uint8_t[]> ram;
+    uint8_t trainer[512] = {0};
+    std::unique_ptr<uint8_t[]> prg;
+    std::unique_ptr<uint8_t[]> chr;
+    std::unique_ptr<uint8_t[]> ram;
 };
 
 #endif
