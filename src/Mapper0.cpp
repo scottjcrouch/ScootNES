072af9aaed519635c4b3a5a58903c554066fb2b3
Scott J. Crouch <scottjcrouch@gmail.com>
2017-02-15 17:15:30 +1100

    Fix mapper0 readPrg ignoring ram

--- src/Mapper0.cpp
+++ src/Mapper0.cpp
@@ -3,8 +3,17 @@
 #include <Mapper0.h>
 
 uint8_t Mapper0::readPrg(uint16_t addr) {
-    int index = addr % cartMemory.prg.size();
-    return cartMemory.prg[index];
+    if (addr >= 0x8000) {
+	int index = addr % cartMemory.prg.size();
+	return cartMemory.prg[index];
+    }
+    else if (addr >= 0x6000) {
+	int index = addr % 0x2000;
+	return cartMemory.ram[index];
+    }
+    else {
+	return 0;
+    }
 }
 
 uint8_t Mapper0::readChr(uint16_t addr) {
