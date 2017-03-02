2b803a4d6e2f425633eabf2a364ffd9972a1b3d0
Scott J. Crouch <scottjcrouch@gmail.com>
2017-03-02 16:17:33 +1100

    Rewrite divider to change interval without resetting it

--- src/Divider.h
+++ src/Divider.h
@@ -5,23 +5,33 @@ class Divider {
 public:
     Divider(int interval = 1) {
 	setInterval(interval);
+	reset();
     }
     void setInterval(int interval) {
-	this->interval = interval;
-	clockCounter = interval - 1;
+	if (interval < 1) {
+	    resetValue = 0;
+	}
+	else {
+	    resetValue = interval - 1;
+	}
+    }
+    void reset() {
+	clockCounter = resetValue;
     }
     void tick() {
-	clockCounter++;
-	if (clockCounter == interval) {
-	    clockCounter = 0;
+	if (clockCounter == 0) {
+	    clockCounter = resetValue;
+	}
+	else {
+	    --clockCounter;
 	}
     }
     bool hasClocked() {
-	return clockCounter == 0;
+	return clockCounter == resetValue;
     }
 
 private:
-    int interval;
+    int resetValue;
     int clockCounter;
 };
 
