bfb6abbb4276bffa9076053eb597a3f0eedf668d
Scott J. Crouch <scottjcrouch@gmail.com>
2017-03-28 14:50:01 +1100

    Replace existing apu implementation with wrapper for blargg apu

--- src/APU.cpp
+++ src/APU.cpp
@@ -1,245 +1,51 @@
 #include <cstdint>
 
 #include <APU.h>
-#include <Divider.h>
 
-APU::APU() {
-    frameCounter(0xFF);
-    ctrl(0);
+static int null_dmc_reader(void*, uint16_t)
+{
+	return 0x55; // causes dmc sample to be flat
 }
 
-void APU::tick(int cycles) {
-    // TODO: increment counters
+APU::APU()
+{
+	time = 0;
+	frameLength = 29780;
+	apu.dmc_reader(null_dmc_reader, NULL);
+	apu.output(&buf);
+	buf.clock_rate(1789773);
+	buf.sample_rate(96000);
 }
 
-void APU::pulse1Ctrl(uint8_t val) {
-    /* Bitfield: DDLC VVVV
-       Duty (D)
-       envelope loop/length counter halt (L)
-       constant volume/disable envelope (C)
-       volume/envelope period (V) */
-    
-    pulse1Duty = (val >> 6) & 0x3;
-    pulse1LenCountHalt = !!(val & 0x20);
-    pulse1EnvLoopEnable = pulse1LenCountHalt;
-    pulse1EnvDisable = !!(val & 0x10);
-    pulse1Vol = val & 0xF;
-    pulse1EnvPeriod = pulse1Vol + 1;
+void APU::setDmcCallback(int (*f)(void* user_data, uint16_t), void* p)
+{
+	apu.dmc_reader(f, p);
 }
 
-void APU::pulse1RampCtrl(uint8_t val) {
-    /* Bitfield: EPPP NSSS
-       sweep enable (E)
-       sweep period (P)
-       sweep negate (N)
-       sweep shift (S) */
-    
-    pulse1SweepEnable = !!(val & 0x80);
-    pulse1SweepPeriod = ((val >> 4) & 0x7) + 1;
-    pulse1SweepNegate = !!(val & 0x8);
-    pulse1SweepShift = val & 0x7;
-    pulse1SweepReload = true;
+void APU::writeRegister(uint16_t addr, uint8_t data)
+{
+	apu.write_register(clock(), addr, data);
 }
 
-void APU::pulse1FineTune(uint8_t val) {
-    /* Bitfield: TTTT TTTT
-       Timer low (T) */
-
-    pulse1Timer &= 0x0700;
-    pulse1Timer |= val & 0xFF;
-    pulse1TimerDivider.setInterval(pulse1Timer + 1);
+int APU::getStatus()
+{
+	return apu.read_status(clock());
 }
 
-void APU::pulse1CoarseTune(uint8_t val) {
-    /* Bitfield: LLLL LTTT
-       Length counter load (L)
-       timer high (T) */
-
-    pulse1Timer &= 0x00FF;
-    pulse1Timer |= (((int)val & 0x07) << 8);
-    pulse1TimerDivider.setInterval(pulse1Timer + 1);
-    pulse1LenCountLoad = val >> 3;
-    if (pulse1Enable) {
-	pulse1LenCount = lengthTable[pulse1LenCountLoad];
-    }
-    pulse1EnvStart = true;
-    // TODO: restart duty cycle sequence
+void APU::endFrame()
+{
+	time = 0;
+	frameLength ^= 1;
+	apu.end_frame(frameLength);
+	buf.end_frame(frameLength);
 }
 
-void APU::pulse2Ctrl(uint8_t val) {
-    /* Bitfield: DDLC VVVV
-       Duty (D)
-       envelope loop/length counter halt (L)
-       constant volume/disable envelope (C)
-       volume/envelope period (V) */
+long APU::samplesAvailable() const
+{
+	return buf.samples_avail();
 }
 
-void APU::pulse2RampCtrl(uint8_t val) {
-    /* Bitfield: EPPP NSSS
-       sweep enable (E)
-       sweep period (P)
-       sweep negate (N)
-       sweep shift (S) */
-}
-
-void APU::pulse2FineTune(uint8_t val) {
-    /* Bitfield: TTTT TTTT
-       Timer low (T) */
-}
-
-void APU::pulse2CoarseTune(uint8_t val) {
-    /* Bitfield: LLLL LTTT
-       Length counter load (L)
-       timer high (T) */
-}
-
-void APU::triangleCtrl1(uint8_t val) {
-    /* Bitfield: CRRR RRRR
-       Length counter halt / linear counter control (C)
-       linear counter load (R) */
-}
-
-void APU::triangleCtrl2(uint8_t val) {
-    /* Unused */
-}
-
-void APU::triangleFreq1(uint8_t val) {
-    /* Bitfield: TTTT TTTT
-       Timer low (T) */
-}
-
-void APU::triangleFreq2(uint8_t val) {
-    /* Bitfield: LLLL LTTT
-       Length counter load (L)
-       timer high (T) */
-}
-
-void APU::noiseCtrl1(uint8_t val) {
-    /* Bitfield: --LC VVVV
-       Envelope loop / length counter halt (L)
-       constant volume (C)
-       volume/envelope (V) */
-}
-
-void APU::noiseCtrl2(uint8_t val) {
-    /* Unused */
-}
-
-void APU::noiseFreq1(uint8_t val) {
-    /* Bitfield: L--- PPPP
-       Loop noise (L)
-       noise period (P) */
-}
-
-void APU::noiseFreq2(uint8_t val) {
-    /* Bitfield: LLLL L---
-       Length counter load (L) */
-}
-
-void APU::dmcCtrl(uint8_t val) {
-    /* Bitfield: IL-- RRRR
-       IRQ enable (I)
-       loop (L)
-       frequency (R) */
-}
-
-void APU::dmcDA(uint8_t val) {
-    /* Bitfield: -DDD DDDD
-       Load counter (D) */
-}
-
-void APU::dmcAddr(uint8_t val) {
-    /* Bitfield: AAAA AAAA
-       Sample address (A) */
-}
-
-void APU::dmcLen(uint8_t val) {
-    /* Bitfield: LLLL LLLL
-       Sample length (L) */
-}
-
-uint8_t APU::status() {
-    /* Bitfield: IF-D NT21
-       DMC interrupt (I)
-       frame interrupt (F)
-       DMC active (D)
-       length counter > 0 (N/T/2/1) */
-    
-    // for all channels but dmc, if length counter > 0 then return true
-    uint8_t status = 0;
-    status |= dmcInterrupt << 7;
-    status |= frameInterrupt << 6;
-    // set if dmc bytes remaining in buffer is > 1
-    // TODO
-    status |= (noiseLenCount > 0) << 3;
-    status |= (triangleLenCount > 0) << 2;
-    status |= (pulse2LenCount > 0) << 1;
-    status |= (pulse1LenCount > 0);
-
-    // clear frame interrupt flag (NOTE: if interrupt flag set at same
-    // time as read, would normally return true and not be cleared)
-    frameInterrupt = false;
-    
-    return status;
-}
-
-void APU::ctrl(uint8_t val) {
-    /* Bitfield: ---D NT21
-       Enable DMC (D)
-       Enable Noise (N)
-       Enable Triangle (T)
-       Enable Pulse Channels (2/1) */
-
-    dmcEnable = !!(val & 0x10);
-    noiseEnable = !!(val & 0x08);
-    triangleEnable = !!(val & 0x04);
-    pulse2Enable = !!(val & 0x02);
-    pulse1Enable = !!(val & 0x01);
-
-    // reset length counters
-    if (!noiseEnable) {
-	noiseLenCount = 0;
-    }
-    if (!triangleEnable) {
-	triangleLenCount = 0;
-    }
-    if (!pulse1Enable) {
-	pulse1LenCount = 0;
-    }
-    if (!pulse2Enable) {
-	pulse2LenCount = 0;
-    }
-
-    // clear dmc interrupt flag
-    dmcInterrupt = false;
-
-    if (dmcEnable) {
-	// restart sample only if buffer empty
-	// TODO
-    }
-    else {
-	// flush buffer and silence it
-	// TODO
-    }
-}
-
-void APU::frameCounter(uint8_t val) {
-    /* Bitfield: MI-- ----
-       Mode (M, 0 = 4-step, 1 = 5-step)
-       IRQ inhibit flag (I) */
-
-    mode = (val & 0x80) ? FIVE_STEP : FOUR_STEP;
-    inhibitIrq = !!(val & 0x40);
-    
-    if (inhibitIrq) {
-	frameInterrupt = false;
-    }
-
-    // reset divider and sequences
-    // TODO
-
-    if (mode == FIVE_STEP)
-    {
-	// clock once
-    }
+long APU::readSamples(sample_t* buffer, long bufferSize)
+{
+	return buf.read_samples(buffer, bufferSize);
 }
--- src/APU.h
+++ src/APU.h
@@ -3,93 +3,29 @@
 
 #include <cstdint>
 
-#include <Divider.h>
-
-static const int lengthTable[0x20] = {
-    10, 254, 20,  2, 40,  4, 80,  6, 160,  8, 60, 10, 14, 12, 26, 14,
-    12,  16, 24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30};
+#include "nes_apu/Nes_Apu.h"
+#include "nes_apu/Blip_Buffer.h"
 
 class APU {
 public:
-    APU();
-
-    void tick(int);
-
-    /** Mapped access registers **/
-
-    // square wave 1 (0x4000 - 0x4003)
-    void pulse1Ctrl(uint8_t);
-    void pulse1RampCtrl(uint8_t);
-    void pulse1FineTune(uint8_t);
-    void pulse1CoarseTune(uint8_t);
-    // square wave 2 (0x4004 - 0x4007)
-    void pulse2Ctrl(uint8_t);
-    void pulse2RampCtrl(uint8_t);
-    void pulse2FineTune(uint8_t);
-    void pulse2CoarseTune(uint8_t);
-    // triangle wave (0x4008 - 0x400B)
-    void triangleCtrl1(uint8_t);
-    void triangleCtrl2(uint8_t);
-    void triangleFreq1(uint8_t);
-    void triangleFreq2(uint8_t);
-    // noise generator (0x400C - 0x400F)
-    void noiseCtrl1(uint8_t);
-    void noiseCtrl2(uint8_t);
-    void noiseFreq1(uint8_t);
-    void noiseFreq2(uint8_t);
-    // delta modulation (0x4010 - 0x4013)
-    void dmcCtrl(uint8_t);
-    void dmcDA(uint8_t);
-    void dmcAddr(uint8_t);
-    void dmcLen(uint8_t);
-    // status (0x4015)
-    uint8_t status();
-    void ctrl(uint8_t);
-    // frame counter (0x4017)
-    void frameCounter(uint8_t);
-
+	APU();
+	void setDmcCallback(int (*callback)(void* user_data, uint16_t), void* user_data = NULL);
+	void writeRegister(uint16_t, uint8_t data);
+	int getStatus();
+	// End a 1/60 sound frame
+	void endFrame();
+	// Number of samples in buffer
+	long samplesAvailable() const;
+	// Read at most 'count' samples and return number of samples actually read
+	typedef blip_sample_t sample_t;
+	long readSamples(sample_t* buffer, long bufferSize);
+	
 private:
-    enum ApuMode {
-	FOUR_STEP,
-	FIVE_STEP,
-    };
-
-    ApuMode mode;
-
-    bool inhibitIrq;
-    bool frameInterrupt;
-    bool dmcInterrupt;
-
-    bool dmcEnable;
-    bool noiseEnable;
-    bool triangleEnable;
-    bool pulse1Enable;
-    bool pulse2Enable;
-
-    int noiseLenCount;
-    int triangleLenCount;
-    int pulse1LenCount;
-    int pulse2LenCount;
-
-    int pulse1Duty;
-    bool pulse1LenCountHalt;
-    bool pulse1EnvLoopEnable;
-    bool pulse1EnvDisable;
-    int pulse1Vol;
-    int pulse1EnvPeriod;
-
-    bool pulse1SweepEnable;
-    int pulse1SweepPeriod;
-    bool pulse1SweepNegate;
-    int pulse1SweepShift;
-    bool pulse1SweepReload;
-
-    int pulse1Timer;
-
-    int pulse1LenCountLoad;
-    bool pulse1EnvStart;
-
-    Divider pulse1TimerDivider;
+	Nes_Apu apu;
+	Blip_Buffer buf;
+	blip_time_t time;
+	blip_time_t frameLength;
+	blip_time_t clock() { return time += 4; }
 };
 
 #endif
