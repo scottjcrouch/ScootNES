28bd1dfb14618106612ea1b872f852c0c0e84faa
Scott J. Crouch <scottjcrouch@gmail.com>
2017-05-09 17:19:52 +1000

    Rewrite cpu read and write methods for readability

--- src/Console.cpp
+++ src/Console.cpp
@@ -50,37 +50,27 @@ uint8_t Console::cpuRead(uint16_t addr) {
         return cpuRam[addr % 0x800];
     }
     else if (addr < 0x4000) {
-        int port = addr % 8;
-        switch (port) {
-        case 0: // 0x2000
-            return cpuBusMDR;
-        case 1: // 0x2001
-            return cpuBusMDR;
-        case 2: // 0x2002
-            return ppu.getSTATUS();
-        case 3: // 0x2003
-            return cpuBusMDR;
-        case 4: // 0x2004
-            return ppu.getOAMDATA();
-        case 5: // 0x2005
-            return cpuBusMDR;
-        case 6: // 0x2006
-            return cpuBusMDR;
-        case 7: // 0x2007
-            return ppu.getDATA();
+        int registerAddr = addr & 0x2007;
+        switch (registerAddr) {
+        case 0x2000: return cpuBusMDR;
+        case 0x2001: return cpuBusMDR;
+        case 0x2002: return ppu.getSTATUS();
+        case 0x2003: return cpuBusMDR;
+        case 0x2004: return ppu.getOAMDATA();
+        case 0x2005: return cpuBusMDR;
+        case 0x2006: return cpuBusMDR;
+        case 0x2007: return ppu.getDATA();
+        }
+    }
+    else if (addr < 0x4018) {
+	switch (addr) {
+        case 0x4015: return apu.getStatus();
+	case 0x4016: return controller1.poll();
+	case 0x4017: return 0; // TODO: controller 2
         }
     }
     else if (addr < 0x4020) {
-	switch (addr) {
-        case 0x4015:
-	    return apu.getStatus();
-	case 0x4016:
-            return controller1.poll();
-	case 0x4017: // TODO: controller 2
-            return 0;
-	default: // disabled/unused APU test registers
-            return cpuBusMDR;
-        }
+	return cpuBusMDR; // disabled/unused APU test registers
     }
     else {
         return cart.readPrg(addr);
@@ -93,47 +83,33 @@ void Console::cpuWrite(uint16_t addr, uint8_t value) {
         cpuRam[addr % 0x800] = value;
     }
     else if (addr < 0x4000) {
-        int port = addr % 8;
-        switch (port) {
-        case 0: // 0x2000
-            ppu.setCTRL(value);
-            break;
-        case 1: // 0x2001
-            ppu.setMASK(value);
-            break;
-        case 2: // 0x2002
-            break;
-        case 3: // 0x2003
-            ppu.setOAMADDR(value);
-            break;
-        case 4: // 0x2004
-            ppu.setOAMDATA(value);
-            break;
-        case 5: // 0x2005
-            ppu.setSCROLL(value);
-            break;
-        case 6: // 0x2006
-            ppu.setADDR(value);
-            break;
-        case 7: // 0x2007
-            ppu.setDATA(value);
-            break;
+        int registerAddr = addr & 0x2007;
+        switch (registerAddr) {
+        case 0x2000: ppu.setCTRL(value); break;
+        case 0x2001: ppu.setMASK(value); break;
+        case 0x2002: break; // ppu status, read-only
+        case 0x2003: ppu.setOAMADDR(value); break;
+        case 0x2004: ppu.setOAMDATA(value); break;
+        case 0x2005: ppu.setSCROLL(value); break;
+        case 0x2006: ppu.setADDR(value); break;
+        case 0x2007: ppu.setDATA(value); break;
         }
     }
-    else if (addr < 0x4020) {
-	if (addr == 0x4014) {
+    else if (addr < 0x4018) {
+	switch (addr) {
+	case 0x4014: {
 	    uint16_t startAddr = ((uint16_t)value) << 8;
 	    for (int i = 0; i < 256; ++i) {
 		ppu.setOAMDATA(cpuRead(startAddr + i));
 	    }
 	    cpu.suspend(514);
+	} break;
+	case 0x4016: controller1.setStrobe(!!(value & 0x1)); break;
+	default: apu.writeRegister(addr, value); break;
 	}
-	else if (addr == 0x4016) {
-	    controller1.setStrobe(!!(value & 0x1));
-	}
-	else if (addr < 0x4018) {
-	    apu.writeRegister(addr, value);
-	}
+    }
+    else if (addr < 0x4020) {
+	return; // disabled/unused APU test registers
     }
     else {
         cart.writePrg(addr, value);
